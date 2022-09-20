799a6551bda4590e0cb238e16d051f47f66f5b94
Willian <willian.alves.b15@gmail.com>
2022-09-20 17:37:01 -0300

    feat: req01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+
+def mock_products():
+    return Product(
+        id=1,
+        nome_do_produto="Nicotine Polacrilex",
+        nome_da_empresa="Target Corporation",
+        data_de_fabricacao="2021-02-18",
+        data_de_validade="2023-09-17",
+        numero_de_serie="CR25 1551 4467 2549 4402 1",
+        instrucoes_de_armazenamento="instrucao 1",
+    )
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = mock_products()
+    assert product.id == 1
+    assert product.nome_do_produto == "Nicotine Polacrilex"
+    assert product.nome_da_empresa == "Target Corporation"
+    assert product.data_de_fabricacao == "2021-02-18"
+    assert product.data_de_validade == "2023-09-17"
+    assert product.numero_de_serie == "CR25 1551 4467 2549 4402 1"
+    assert product.instrucoes_de_armazenamento == "instrucao 1"
