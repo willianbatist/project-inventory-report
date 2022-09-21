e3e667f4192d608804a759b93bc09a863cf5b4ff
Willian <willian.alves.b15@gmail.com>
2022-09-21 19:24:49 -0300

    feat: req06

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,9 +1,11 @@
 import csv
+import json
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
-def render_files(path, str):
+def render_files(path, type: str):
     reader = str(path).split(".")[-1]
     if reader == "csv":
         with open(path, encoding="utf8") as file:
@@ -11,13 +13,19 @@ def render_files(path, str):
                 file, delimiter=",", quotechar='"'
             )
             *data, = reader
+    if reader == "json":
+        with open(path) as file:
+            data = json.load(file)
+    if reader == "xml":
+        with open(path) as file:
+            data = xmltodict.parse(file.read())["dataset"]["record"]
     return data
 
 
 class Inventory:
     @staticmethod
-    def import_data(path, str):
-        dado = render_files(path, str)
+    def import_data(path, type: str):
+        dado = render_files(path, type)
         if type == "simples":
             return SimpleReport.generate(dado)
         if type == "completo":
