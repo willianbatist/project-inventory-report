7b25b61f72ae82108c01d2b9b5b5a901f5a71fb4
Willian <willian.alves.b15@gmail.com>
2022-09-20 23:31:37 -0300

    fix: req03.2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,37 +1,6 @@
 from datetime import datetime
 
 
-# test = [
-#      {
-#        "id": 1,
-#        "nome_do_produto": "CADEIRA",
-#        "nome_da_empresa": "Forces of Nature",
-#        "data_de_fabricacao": "2022-04-04",
-#        "data_de_validade": "2023-02-09",
-#        "numero_de_serie": "FR48",
-#        "instrucoes_de_armazenamento": "Conservar em local fresco"
-#      },
-#      {
-#        "id": 2,
-#        "nome_do_produto": "CADEIRA",
-#        "nome_da_empresa": "Forces of Nature",
-#        "data_de_fabricacao": "2028-04-04",
-#        "data_de_validade": "2025-02-09",
-#        "numero_de_serie": "FR48",
-#        "instrucoes_de_armazenamento": "Conservar em local fresco"
-#      },
-#      {
-#        "id": 3,
-#        "nome_do_produto": "CADEIRA",
-#        "nome_da_empresa": "Forces of Nature",
-#        "data_de_fabricacao": "2013-04-04",
-#        "data_de_validade": "2014-02-09",
-#        "numero_de_serie": "FR48",
-#        "instrucoes_de_armazenamento": "Conservar em local fresco"
-#      }
-#    ]
-
-
 class SimpleReport:
     @staticmethod
     def generate(productList):
@@ -62,6 +31,3 @@ class SimpleReport:
             f"Data de validade mais próxima: {date_val}\n"
             f"Empresa com mais produtos: {company}"
         )
-
-
-# print(SimpleReport.generate(test))
